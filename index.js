047f976d203c5350bab1fab752100243ed1d8ab1
salawudeen ibrahim <salawudeen_i@yahoo.com>
2019-02-18 16:27:46 +0100

    changed style a bit oo

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 'use strict'
 
 
+      /* let fadeOutFrom = 10;
+
+      const fadeout = element =>{
+        element = "dd";
+        var target = document.getElementById(element)
+        var newSetting = fadeOutFrom / 10 ;
+        target.style.opacity = newSetting;
+        fadeOutFrom--;
+        if (fadeOutFrom == 0) {
+          target.style.opacity = 0;
+          target.style.display = none;
+          clearTimeout(loopTimer);
+          fadeOutFrom = 10;
+          return false;
+        }
+
+        const loopTimer = setTimeout('fadeout(\''+element+'\')', 500);
+      }
+
+      var myVar = setInterval(fadeout, 500); */
 
 const http = require('http');
 const express = require('express');
